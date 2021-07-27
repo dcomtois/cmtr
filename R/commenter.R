c673080e72eebfbae6d449e1c5050b0d3df2f707
Dominic Comtois <dominic.comtois@gmail.com>
2021-07-27 03:03:34 -0400

    initial commit

---
+++ R/commenter.R
@@ -0,0 +1,19 @@
+#' Comment Selection
+#'
+#' Call this function as an RStudio addin to comment/uncomment selection
+#'
+#' @importFrom rstudioapi getActiveDocumentContext primary_selection
+#' @export
+commenter <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  selection <- rstudioapi::primary_selection(context)
+  re1 <- "^(\\s*)(\\/\\* ?)(.+?)( ?\\*\\/)(\\s*)$"
+  if (grepl(re1, selection$text)) {
+    text <- sub(re1, "\\1\\3\\5", selection$text)
+  } else {
+    text <- sub("(.+)(\\r?\\n?\\s?)$", "/* \\1 */\\2", selection$text)
+  }
+  rstudioapi::insertText(
+    location = selection$range,
+    text = text)
+}
